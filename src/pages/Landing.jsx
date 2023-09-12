e6e20bf6c74bc068f92d73b9387dd5fc06f18f0f
MrEmin <emuhammet.ozdemir@gmail.com>
2023-09-12 16:17:42 +0300

    Landing Page - Structure

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,6 +2,27 @@ import logo from '../assets/images/logo.svg'
 import main from '../assets/images/main.svg'
 
 const Landing = () => {
-  return <h1>Landing page</h1>
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt='jobster logo' className='logo' />
+      </nav>
+      <div className='container page'>
+        {/* info */}
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Master cleanse air plant authentic YOLO readymade DIY tumblr artisan
+            XOXO occupy VHS. Man bun DSA swag, thundercats marfa tofu aesthetic
+            neutral milk hotel. Tumblr microdosing hoodie migas jianbing hella.
+          </p>
+          <button className='btn btn-hero'>Login/Register</button>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </main>
+  )
 }
 export default Landing
