7f2c521a0a2fa0432c1556cd4fad224515241187
MrEmin <emuhammet.ozdemir@gmail.com>
2023-10-26 15:34:10 +0300

    Create Job Request

--- src/pages/dashboard/AddJob.jsx
+++ src/pages/dashboard/AddJob.jsx
@@ -2,7 +2,11 @@ import { FormRow, FormRowSelect } from '../../components'
 import Wrapper from '../../assets/wrappers/DashboardFormPage'
 import { useSelector, useDispatch } from 'react-redux'
 import { toast } from 'react-toastify'
-import { clearValues, handleChange } from '../../features/job/jobSlice'
+import {
+  clearValues,
+  handleChange,
+  createJob,
+} from '../../features/job/jobSlice'
 
 const AddJob = () => {
   const {
@@ -27,6 +31,7 @@ const AddJob = () => {
       toast.error('Please fill out all fields')
       return
     }
+    dispatch(createJob({ position, company, jobLocation, jobType, status }))
   }
 
   const handleJobInput = (e) => {
