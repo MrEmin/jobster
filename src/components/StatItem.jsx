2cde28d6bc4f79b215520aecf7cd400e98a15bd7
MrEmin <emuhammet.ozdemir@gmail.com>
2023-11-01 23:06:43 +0300

    Stat Item

--- src/components/StatItem.jsx
+++ src/components/StatItem.jsx
@@ -1,4 +1,14 @@
-const StatItem = () => {
-  return <div>StatItem</div>
+import Wrapper from '../assets/wrappers/StatItem'
+
+const StatItem = ({ title, count, icon, color, bg }) => {
+  return (
+    <Wrapper color={color} bcg={bg}>
+      <header>
+        <span className='count'>{count}</span>
+        <span className='icon'>{icon}</span>
+      </header>
+      <h5 className='title'>{title}</h5>
+    </Wrapper>
+  )
 }
 export default StatItem
