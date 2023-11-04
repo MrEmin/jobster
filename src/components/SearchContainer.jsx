d813e2c48989d2fb2ce48dbff9391c08c14f16b9
MrEmin <emuhammet.ozdemir@gmail.com>
2023-11-04 09:55:50 +0300

    Handle Change and Clear Filters

--- src/components/SearchContainer.jsx
+++ src/components/SearchContainer.jsx
@@ -1,6 +1,7 @@
 import { FormRow, FormRowSelect } from '.'
 import Wrapper from '../assets/wrappers/SearchContainer'
 import { useSelector, useDispatch } from 'react-redux'
+import { clearFilters, handleChange } from '../features/allJobs/allJobsSlice'
 
 const SearchContainer = () => {
   const { isLoading, search, searchStatus, searchType, sort, sortOptions } =
@@ -8,9 +9,12 @@ const SearchContainer = () => {
   const { jobTypeOptions, statusOptions } = useSelector((store) => store.job)
   const dispatch = useDispatch()
 
-  const handleSearch = (e) => {}
+  const handleSearch = (e) => {
+    dispatch(handleChange({ name: e.target.name, value: e.target.value }))
+  }
   const handleSubmit = (e) => {
     e.preventDefault()
+    dispatch(clearFilters())
   }
 
   return (
