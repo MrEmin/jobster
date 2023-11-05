736ed25dce19e7f1e2c4b878db0d6bd8301e609c
MrEmin <emuhammet.ozdemir@gmail.com>
2023-11-05 14:14:53 +0300

    Refactor to LocalSearch

--- src/components/SearchContainer.jsx
+++ src/components/SearchContainer.jsx
@@ -2,15 +2,16 @@ import { FormRow, FormRowSelect } from '.'
 import Wrapper from '../assets/wrappers/SearchContainer'
 import { useSelector, useDispatch } from 'react-redux'
 import { clearFilters, handleChange } from '../features/allJobs/allJobsSlice'
+import { useState, useMemo } from 'react'
 
 const SearchContainer = () => {
+  const [localSearch, setLocalSearch] = useState('')
   const { isLoading, search, searchStatus, searchType, sort, sortOptions } =
     useSelector((store) => store.allJobs)
   const { jobTypeOptions, statusOptions } = useSelector((store) => store.job)
   const dispatch = useDispatch()
 
   const handleSearch = (e) => {
-    if (isLoading) return
     dispatch(handleChange({ name: e.target.name, value: e.target.value }))
   }
   const handleSubmit = (e) => {
@@ -27,8 +28,8 @@ const SearchContainer = () => {
           <FormRow
             type='text'
             name='search'
-            value={search}
-            handleChange={handleSearch}
+            value={localSearch}
+            handleChange={(e) => setLocalSearch(e.target.value)}
           />
 
           {/* search by status */}
